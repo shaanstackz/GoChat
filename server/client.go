c6c533a8ea9248b563ddc02e1547c9b4b3a1bc47
Shaan Kohli <shaan.kohli@torontomu.ca>
2025-12-19 10:32:52 -0500

    added commands

--- server/client.go
+++ server/client.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strings"
 )
 
 type Client struct {
@@ -13,15 +14,13 @@ type Client struct {
 	username string
 }
 
-
 func NewClient(conn net.Conn, server *Server) *Client {
 	return &Client{
-		conn:     conn,
-		server:   server,
-		send: make(chan string, 10),
+		conn:   conn,
+		server: server,
+		send:   make(chan string, 10), 
 	}
 }
-
 
 func (c *Client) Read() {
 	defer func() {
@@ -36,18 +35,50 @@ func (c *Client) Read() {
 	if !scanner.Scan() {
 		return
 	}
-	c.username = scanner.Text()  
+	c.username = scanner.Text()
 
 	c.server.broadcast <- fmt.Sprintf("🔵 %s joined the chat", c.username)
 
 	for scanner.Scan() {
 		msg := scanner.Text()
+
+		if msg == "/users" {
+			userList := c.server.ListUsers()
+			c.send <- "Connected users: " + userList
+			continue
+		}
+
+		if strings.HasPrefix(msg, "/msg ") {
+			parts := strings.SplitN(msg, " ", 3)
+			if len(parts) < 3 {
+				c.send <- "Usage: /msg username message"
+				continue
+			}
+			targetName := parts[1]
+			privateMsg := parts[2]
+			if !c.server.SendPrivate(targetName, fmt.Sprintf("[PM from %s] %s", c.username, privateMsg)) {
+				c.send <- "User not found: " + targetName
+			}
+			continue
+		}
+
+		if strings.HasPrefix(msg, "/nick ") {
+			newName := strings.TrimSpace(strings.TrimPrefix(msg, "/nick "))
+			if newName == "" {
+				c.send <- "Usage: /nick newname"
+				continue
+			}
+			oldName := c.username
+			c.username = newName
+			c.server.broadcast <- fmt.Sprintf("🔄 %s changed name to %s", oldName, newName)
+			continue
+		}
+
 		formatted := fmt.Sprintf("[%s] %s", c.username, msg)
-		fmt.Println("READ:", formatted)   
+		fmt.Println("READ:", formatted)      
 		c.server.broadcast <- formatted
 	}
 }
-
 
 func (c *Client) Write() {
 	writer := bufio.NewWriter(c.conn)
--- server/server.go
+++ server/server.go
@@ -6,17 +6,17 @@ import (
 )
 
 type Server struct {
-	clients   map[*Client]bool
-	broadcast chan string
-	register  chan *Client
+	clients    map[*Client]bool
+	broadcast  chan string
+	register   chan *Client
 	unregister chan *Client
 }
 
 func NewServer() *Server {
 	return &Server{
-		clients:   make(map[*Client]bool),
-		broadcast: make(chan string),
-		register:  make(chan *Client),
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan string),
+		register:   make(chan *Client),
 		unregister: make(chan *Client),
 	}
 }
@@ -26,16 +26,17 @@ func (s *Server) Run() {
 		select {
 		case client := <-s.register:
 			s.clients[client] = true
-			fmt.Println("Client connected")
+			fmt.Println("Client connected:", client.username)
 
 		case client := <-s.unregister:
 			if _, ok := s.clients[client]; ok {
 				delete(s.clients, client)
 				close(client.send)
-				fmt.Println("Client disconnected")
+				fmt.Println("Client disconnected:", client.username)
 			}
 
 		case message := <-s.broadcast:
+			fmt.Println("SERVER broadcasting:", message)
 			for client := range s.clients {
 				select {
 				case client.send <- message:
@@ -48,6 +49,27 @@ func (s *Server) Run() {
 	}
 }
 
+func (s *Server) ListUsers() string {
+	users := ""
+	for client := range s.clients {
+		users += client.username + ", "
+	}
+	if len(users) > 2 {
+		users = users[:len(users)-2] // remove trailing comma
+	}
+	return users
+}
+
+func (s *Server) SendPrivate(username, msg string) bool {
+	for client := range s.clients {
+		if client.username == username {
+			client.send <- msg
+			return true
+		}
+	}
+	return false
+}
+
 func Start(addr string) {
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
@@ -58,18 +80,17 @@ func Start(addr string) {
 	fmt.Println("Chat server started on", addr)
 
 	server := NewServer()
-	go server.Run()  
+	go server.Run()
 
 	for {
-		conn, err := listener.Accept()  
+		conn, err := listener.Accept()
 		if err != nil {
 			continue
 		}
 
-		client := NewClient(conn, server)   
-		server.register <- client          
-		go client.Read()                    
+		client := NewClient(conn, server)
+		server.register <- client           
 		go client.Write()                   
+		go client.Read()                    
 	}
 }
-
