49a3aa608cfb2ddc7e96aa4f502d8e02892d9d0c
Shaan Kohli <shaan.kohli@torontomu.ca>
2026-01-15 15:22:41 -0500

    fixed filesharing

--- server/client.go
+++ server/client.go
@@ -1,157 +1,80 @@
-package server
+package main
 
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
+	"os"
 	"strconv"
 	"strings"
-	"os"
-	"io"
 )
 
 type Client struct {
 	conn     net.Conn
-	server   *Server
 	send     chan string
+	server   *Server
 	username string
-	room     string
 }
 
 func NewClient(conn net.Conn, server *Server) *Client {
+	reader := bufio.NewReader(conn)
+	conn.Write([]byte("Enter username: "))
+	name, _ := reader.ReadString('\n')
+
 	return &Client{
-		conn:   conn,
-		server: server,
-		send:   make(chan string, 10),
+		conn:     conn,
+		send: make(chan string, 20),
+		server:   server,
+		username: strings.TrimSpace(name),
 	}
 }
 
 func (c *Client) Read() {
-	defer func() {
-		c.server.RemoveClient(c)
-		c.conn.Close()
-	}()
-
 	scanner := bufio.NewScanner(c.conn)
-	fmt.Fprintln(c.conn, "Enter username:")
-	if !scanner.Scan() {
-		return
-	}
-	c.username = scanner.Text()
-	c.room = "lobby"
-	c.server.AddClient(c, "lobby")
 
 	for scanner.Scan() {
 		msg := scanner.Text()
 
-		if msg == "/users" {
-			c.send <- c.server.ListUsers()
-			continue
-		}
+		if strings.HasPrefix(msg, "/sendfile ") {
+			filename := strings.TrimSpace(strings.TrimPrefix(msg, "/sendfile "))
+			c.send <- "READY"
 
-		if msg == "/rooms" {
-			c.send <- c.server.ListRooms()
-			continue
-		}
+			reader := bufio.NewReader(c.conn)
+			header, _ := reader.ReadString('\n')
+			parts := strings.Split(strings.TrimSpace(header), " ")
 
-		if strings.HasPrefix(msg, "/join ") {
-			room := strings.TrimSpace(strings.TrimPrefix(msg, "/join "))
-			c.server.MoveClient(c, room)
-			continue
-		}
-
-		if strings.HasPrefix(msg, "/msg ") {
-			parts := strings.SplitN(msg, " ", 3)
-			if len(parts) < 3 {
-				continue
-			}
-			c.server.PrivateMessage(c.username, parts[1], parts[2])
-			continue
-		}
-
-		if strings.HasPrefix(msg, "/search ") {
-			keyword := strings.TrimSpace(strings.TrimPrefix(msg, "/search "))
-			results := c.server.Search(c.room, keyword)
-			for _, r := range results {
-				c.send <- r
-			}
-			continue
-		}
-
-		if strings.HasPrefix(msg, "/edit ") {
-			parts := strings.SplitN(msg, " ", 3)
-			if len(parts) < 3 {
-				continue
-			}
-			id, _ := strconv.Atoi(parts[1])
-			c.server.EditMessage(c, id, parts[2])
-			continue
-		}
-
-		if strings.HasPrefix(msg, "/delete ") {
-			id, _ := strconv.Atoi(strings.TrimSpace(strings.TrimPrefix(msg, "/delete ")))
-			c.server.DeleteMessage(c, id)
-			continue
-		}
-
-		if strings.HasPrefix(msg, "FILE ") {
-			parts := strings.Split(msg, " ")
-			filename := parts[1]
 			size, _ := strconv.Atoi(parts[2])
-
 			buf := make([]byte, size)
 			io.ReadFull(c.conn, buf)
 
 			os.WriteFile("uploads/"+filename, buf, 0644)
-
-			c.server.BroadcastSystem(
-				c.room,
-				fmt.Sprintf("%s uploaded file %s (%d bytes)", c.username, filename, size),
-			)
-			continue
-		}
-
-
-		if strings.HasPrefix(msg, "/sendfile ") {
-			filename := strings.TrimSpace(strings.TrimPrefix(msg, "/sendfile "))
-			data, err := os.ReadFile(filename)
-			if err != nil {
-				c.send <- "File not found"
-				continue
-			}
-
-			header := fmt.Sprintf("FILE %s %d\n", filename, len(data))
-			c.conn.Write([]byte(header))
-			c.conn.Write(data)
-
-			c.send <- "File sent: " + filename
+			c.server.broadcast <- "[SYSTEM] " + c.username + " uploaded " + filename
 			continue
 		}
 
 		if strings.HasPrefix(msg, "/getfile ") {
 			filename := strings.TrimSpace(strings.TrimPrefix(msg, "/getfile "))
-			path := "uploads/" + filename
-
-			data, err := os.ReadFile(path)
+			data, err := os.ReadFile("uploads/" + filename)
 			if err != nil {
 				c.send <- "File not found"
 				continue
 			}
-
 			header := fmt.Sprintf("FILE %s %d\n", filename, len(data))
 			c.conn.Write([]byte(header))
 			c.conn.Write(data)
 			continue
 		}
 
-		c.server.Broadcast(c, msg)
+		c.server.broadcast <- "[" + c.username + "] " + msg
 	}
+
+	c.server.unregister <- c
+	c.conn.Close()
 }
 
 func (c *Client) Write() {
-	writer := bufio.NewWriter(c.conn)
 	for msg := range c.send {
-		fmt.Fprintln(writer, msg)
-		writer.Flush()
+		c.conn.Write([]byte(msg + "\n"))
 	}
 }
--- server/server.go
+++ server/server.go
@@ -1,155 +1,59 @@
-package server
+package main
 
 import (
 	"fmt"
 	"net"
-	"strings"
-	"time"
+	"os"
 )
 
-type Message struct {
-	ID      int
-	Sender  string
-	Content string
-	Time    string
-}
-
 type Server struct {
-	clients map[*Client]bool
-	rooms   map[string]map[*Client]bool
-	history map[string][]Message
-	nextID  int
+	clients    map[*Client]bool
+	register   chan *Client
+	unregister chan *Client
+	broadcast  chan string
 }
 
 func NewServer() *Server {
 	return &Server{
-		clients: make(map[*Client]bool),
-		rooms:   make(map[string]map[*Client]bool),
-		history: make(map[string][]Message),
-		nextID:  1,
+		clients:    make(map[*Client]bool),
+		register:   make(chan *Client, 10),
+		unregister: make(chan *Client, 10),
+		broadcast:  make(chan string, 100),
 	}
 }
 
-func (s *Server) AddClient(c *Client, room string) {
-	s.clients[c] = true
-	if s.rooms[room] == nil {
-		s.rooms[room] = make(map[*Client]bool)
-	}
-	s.rooms[room][c] = true
-	c.send <- "Joined room " + room
-	for _, m := range s.history[room] {
-		c.send <- fmt.Sprintf("[%d] [%s] [%s] %s", m.ID, m.Time, m.Sender, m.Content)
-	}
-	s.BroadcastSystem(room, c.username+" joined")
-}
-
-func (s *Server) RemoveClient(c *Client) {
-	delete(s.clients, c)
-	if s.rooms[c.room] != nil {
-		delete(s.rooms[c.room], c)
-		s.BroadcastSystem(c.room, c.username+" left")
-	}
-	close(c.send)
-}
-
-func (s *Server) MoveClient(c *Client, room string) {
-	if s.rooms[c.room] != nil {
-		delete(s.rooms[c.room], c)
-	}
-	c.room = room
-	s.AddClient(c, room)
-}
-
-func (s *Server) Broadcast(c *Client, content string) {
-	msg := Message{
-		ID:      s.nextID,
-		Sender:  c.username,
-		Content: content,
-		Time:    time.Now().Format("15:04"),
-	}
-	s.nextID++
-	s.history[c.room] = append(s.history[c.room], msg)
-
-	formatted := fmt.Sprintf("[%d] [%s] [%s] %s", msg.ID, msg.Time, msg.Sender, msg.Content)
-	for client := range s.rooms[c.room] {
-		client.send <- formatted
-		if strings.Contains(content, "@"+client.username) {
-			client.send <- "[MENTION] " + c.username + " mentioned you"
+func (s *Server) Run() {
+	for {
+		select {
+		case c := <-s.register:
+			s.clients[c] = true
+			s.broadcast <- "[SYSTEM] " + c.username + " joined"
+		case c := <-s.unregister:
+			delete(s.clients, c)
+			close(c.send)
+			s.broadcast <- "[SYSTEM] " + c.username + " left"
+		case msg := <-s.broadcast:
+			for c := range s.clients {
+				c.send <- msg
+			}
 		}
 	}
 }
 
-func (s *Server) BroadcastSystem(room, msg string) {
-	for client := range s.rooms[room] {
-		client.send <- "[SYSTEM] " + msg
-	}
-}
+func main() {
+	os.MkdirAll("uploads", 0755)
 
-func (s *Server) PrivateMessage(from, to, msg string) {
-	for client := range s.clients {
-		if client.username == to {
-			client.send <- "[PM from " + from + "] " + msg
-			return
-		}
-	}
-}
-
-func (s *Server) ListUsers() string {
-	var users []string
-	for c := range s.clients {
-		users = append(users, c.username)
-	}
-	return "Users: " + strings.Join(users, ", ")
-}
-
-func (s *Server) ListRooms() string {
-	var rooms []string
-	for r := range s.rooms {
-		rooms = append(rooms, r)
-	}
-	return "Rooms: " + strings.Join(rooms, ", ")
-}
-
-func (s *Server) Search(room, keyword string) []string {
-	var results []string
-	for _, m := range s.history[room] {
-		if strings.Contains(m.Content, keyword) {
-			results = append(results,
-				fmt.Sprintf("[%d] [%s] [%s] %s", m.ID, m.Time, m.Sender, m.Content))
-		}
-	}
-	return results
-}
-
-func (s *Server) EditMessage(c *Client, id int, text string) {
-	for i, m := range s.history[c.room] {
-		if m.ID == id && m.Sender == c.username {
-			s.history[c.room][i].Content = text
-			s.BroadcastSystem(c.room, "message edited")
-			return
-		}
-	}
-}
-
-func (s *Server) DeleteMessage(c *Client, id int) {
-	for i, m := range s.history[c.room] {
-		if m.ID == id && m.Sender == c.username {
-			s.history[c.room] = append(s.history[c.room][:i], s.history[c.room][i+1:]...)
-			s.BroadcastSystem(c.room, "message deleted")
-			return
-		}
-	}
-}
-
-func Start(addr string) {
-	ln, _ := net.Listen("tcp", addr)
-	fmt.Println("Server on", addr)
 	server := NewServer()
+	go server.Run()
+
+	ln, _ := net.Listen("tcp", ":9000")
+	fmt.Println("Chat server started on :9000")
 
 	for {
 		conn, _ := ln.Accept()
 		client := NewClient(conn, server)
-		go client.Write()
+		server.register <- client
 		go client.Read()
+		go client.Write()
 	}
 }
